a60de5ec3a611cbd3dc215b045157d580a19db95
Jason Xu <Xinwen31415@gmail.com>
2021-06-30 01:02:38 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Client, TextChannel } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client();
+
+client.on('ready', async () => {
+	const ch = (await client.channels.fetch('859672475121877002')) as TextChannel;
+	const msg = (await ch.messages.fetch()).find((msg) => msg.id === '859673437537828875');
+
+	setInterval(() => {
+		msg!.edit(`The time is ${new Date().toTimeString()}`);
+	}, 5000);
+});
+
+client.login(process.env.DISCORD_TOKEN!);
